fcb12d7a6b7a2349fcafa4f75ba06d2f5fc5eb69
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-29 00:22:51 +0300

    New commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Gain information from an API
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    id = argv[1]
+    data = requests.get(url + f"/todos?userId={id}").json()
+    done = []
+
+    for task in data:
+        if task['completed'] is True:
+            done.append(task)
+
+    name = requests.get(url + f"/users/{id}").json()['name']
+    print(f"Employee {name} is done with tasks({len(done)}/{len(data)})")
+    for task in done:
+        print(f"\t {task['title']}")
