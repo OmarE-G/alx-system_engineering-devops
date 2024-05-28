27c20a99855f7687aa5f88522f236655fc9a1a36
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-29 01:29:25 +0300

    New commit

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,8 +10,13 @@ if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     id = argv[1]
     data = requests.get(url + f"/todos?userId={id}").json()
-    done = []
+    tasks = {}
 
     name = requests.get(url + f"/users/{id}").json()['username']
+    for task in data:
+        tasks[str(id)]['username'] = name
+        tasks[str(id)]["task"] = task["title"]
+        tasks[str(id)]["completed"] = task["completed"]
+        
     with open(f'{id}.json', 'w', newline='') as file:
-        json.dump(data, file)
+        json.dump(tasks, file)
