24345f8ce1fbcd768a7447fab8120be74dc334b0
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-29 00:38:44 +0300

    New commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Gain information from an API
+"""
+
+import requests
+import csv
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    id = argv[1]
+    data = requests.get(url + f"/todos?userId={id}").json()
+    done = []
+
+    name = requests.get(url + f"/users/{id}").json()['name']
+    with open('USER_ID.csv', 'w') as file:
+        for task in data:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            writer.writerow([id, name, task['completed'], task['title']])
