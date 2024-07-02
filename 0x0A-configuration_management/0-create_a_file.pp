8b04f218f263f256c51302a2440694912bf6b533
Omar Essam <omar161157@eng.psu.edu.eg>
2024-07-02 14:51:01 +0300

    New commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,11 @@
 # create a file in /tmp with owner, group, and mode.
 
+$content = 'I love Puppet'
+
 file { '/tmp/school':
-ensure  => file,t
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet'
+  ensure  => file,
+  content => $content,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
 }
